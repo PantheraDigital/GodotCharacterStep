ca4bbc5ee8e20fdeb4fc6a022c233a3abffa15a6
PantheraDigital <101964317+PantheraDigital@users.noreply.github.com>
2025-07-14 23:35:13 -0700

    step interface change
    Step functions are more general use now, no longer using a collider and body. Logic has been changed to accommodate this change in used data. 
    Step up function has also been adjusted to slim the stabilizer ray casts, allowing for usage on smaller surfaces.
    Along with function argument changes the step functions also return a dictionary now instead of just a location. This is to provide more data to the caller allowing them to use it as they need instead of the functions having too much responsibility, limiting function flexibility.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -58,7 +58,7 @@ func _physics_process(delta: float) -> void:
 	var perform_step := false
 	if !move_direction.is_zero_approx():
 		# set up variables
-		var start : Vector3 = Vector3(global_position.x, global_position.y - (_collision_shape_3d.shape.height / 2), global_position.z)
+		var start : Vector3 = global_position
 		var start_offset : Vector3 = Vector3.ZERO
 		var direction : Vector3 = Vector3(velocity.x, 0.0, velocity.z).normalized()
 		var width : float = _collision_shape_3d.shape.radius * 0.5
@@ -82,22 +82,25 @@ func _physics_process(delta: float) -> void:
 			perform_step = true
 			
 	# step_up should come BEFORE move_and_slide
+	# we need to move the player to the ledge before they run into it like a wall
 	# move_and_slide will smooth the movement of the character after step_up
+	var stepped_up := false # reduces step down calls
 	if !move_direction.is_zero_approx() and perform_step:
-		var step : Dictionary = CharacterStep3D.step_up(self, _collision_shape_3d, MAX_STEP_UP)
-		if step and step["normal"].angle_to(Vector3.UP) <= floor_max_angle:
-			# center point of player is in the middle so we need to raise it half of its height to align the bottom to the ground
-			global_position.y = step["point"].y + (_collision_shape_3d.shape.height / 2)
+		var step : Dictionary = CharacterStep3D.step_up(self.get_rid(), global_transform, MAX_STEP_UP, Vector3(velocity.x, 0.0, velocity.z).normalized(), _collision_shape_3d.shape.radius * 0.5, 0.25)
+		if !step.is_empty() and step["normal"].angle_to(Vector3.UP) <= floor_max_angle:
+			global_position.y = step["point"].y
+			stepped_up = true
 	
 	move_and_slide()
 	
 	# step_down should come AFTER move_and_slide
 	# this is because if the player is not grounded but they were grounded before they moved
 	# then they have stepped off a ledge and it should be checked if they can step_down
-	if not is_on_floor() and velocity.y <= 0 and was_on_floor:
-		var step : Vector3 = CharacterStep3D.step_down(self, _collision_shape_3d, MAX_STEP_DOWN)
-		if step != global_position:
-			global_position = step
+	if not stepped_up and not is_on_floor() and velocity.y <= 0 and was_on_floor:
+		var ground_validation : Callable = func(_point : Vector3, _normal : Vector3) -> bool : return _normal.angle_to(Vector3.UP) <= floor_max_angle
+		var step : Dictionary = CharacterStep3D.step_down(self.get_rid(), global_transform, MAX_STEP_DOWN, Vector3(velocity.x, 0.0, velocity.z).normalized(), _collision_shape_3d.shape.radius, ground_validation)
+		if !step.is_empty():
+			global_position = step["point"]
 	
 	# apply_floor_snap should come after all movement
 	if velocity.y <= 0:
@@ -105,9 +108,9 @@ func _physics_process(delta: float) -> void:
 		apply_floor_snap()
 	
 	# smooth camera movement to prevent harsh teleporting when stepping up and down
-	_spring_arm_3d.global_position.x = global_position.x
-	_spring_arm_3d.global_position.y = lerpf(_spring_arm_3d.global_position.y, global_position.y, 0.15)
-	_spring_arm_3d.global_position.z = global_position.z
+	_spring_arm_3d.global_position.x = _camera_pivot.global_position.x
+	_spring_arm_3d.global_position.y = lerpf(_spring_arm_3d.global_position.y, _camera_pivot.global_position.y, 0.15)
+	_spring_arm_3d.global_position.z = _camera_pivot.global_position.z
 
 
 func _unhandled_input(event: InputEvent) -> void:
